3cddb61397c7952199b185cba88acb3c5a387944
lpolon <lpolon@gmail.com>
2020-04-27 17:43:01 -0300

    better test descriptions

--- 1) First Unique character/problem.test.js
+++ 1) First Unique character/problem.test.js
@@ -1,15 +1,20 @@
-import {findFirstUniqueChar} from './problem'
+import { findFirstUniqueChar } from './problem'
 
 describe('the findFirstUniqueChar function', () => {
-  it('test 1', () => {
-    const string = 'aaron';
-    const result = findFirstUniqueChar(string);
+  it('test 1: "aaron" returns 2', () => {
+    const string = 'aaron'
+    const result = findFirstUniqueChar(string)
 
     expect(result).toStrictEqual(2)
   })
-  it('test2', () => {
-    const string = 'aabbcc';
-    const result = findFirstUniqueChar(string);
+  it('test2: "leonardo" returns 0', () => {
+    const string = 'leonardo'
+    const result = findFirstUniqueChar(string)
+    expect(result).toStrictEqual(0)
+  })
+  it('test3: "aabbcc" returns -1', () => {
+    const string = 'aabbcc'
+    const result = findFirstUniqueChar(string)
 
     expect(result).toStrictEqual(-1)
   })
