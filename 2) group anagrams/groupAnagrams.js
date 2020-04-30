b2ddb796d372099b7043a4e64821f10d9badfda3
Leonardo Polon <lpolon@gmail.com>
2020-04-30 01:34:43 -0300

    exercise 2 wip

--- 2) group anagrams/groupAnagrams.js
+++ 2) group anagrams/groupAnagrams.js
@@ -1,3 +1,11 @@
 export const groupAnagrams = (stringArr) => {
-  
+  const hashTable = stringArr.reduce((ht, word) => {
+    const sortedWord = word.split('').sort((a, b) => a.localeCompare(b)).join('') //?
+    if (ht[sortedWord]) ht[sortedWord] += 1;
+    else ht[sortedWord] = 1;
+    return ht
+  }, {})
+  console.log(hashTable)
 }
+
+groupAnagrams(['cba', 'bca', 'ba'])
