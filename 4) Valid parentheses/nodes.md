16bae4097a778a89f9e4796b923ed702566a493e
Leonardo Polon <lpolon@gmail.com>
2020-05-03 21:05:08 -0300

    unguided pseudo-code plan done

--- 4) Valid parentheses/nodes.md
+++ 4) Valid parentheses/nodes.md
@@ -2,3 +2,9 @@
 
 ## unguided attempt:
 i couldn't figure out anything. I got the hint about using a stack (first in, last out)
+
+pseudo-code:
+- turn input into array as a stack (stackInput)
+- remove last item. Is it a closing parentheses? if yes add it to a 2nd stack (stackHold)
+- if no, get the last item of the stackHold. Is it a matching pair? if no, return false
+- if yes -> repeat.
--- 4) Valid parentheses/solution.test.js
+++ 4) Valid parentheses/solution.test.js
@@ -1,19 +1,19 @@
-import {isValidParentheses} from './solution'
+import {isValidParentheses, Stack} from './solution'
 
 describe('isValidParentheses() determines if a string of only "(", ")", "[", "]", "{","}" is valid', () => {
   test('"()" returns true', () => {
     expect(isValidParentheses('()')).toStrictEqual(true)
   })
-  test('"()[]{}" returns true', () => {
+  test.skip('"()[]{}" returns true', () => {
     expect(isValidParentheses('()[]{}')).toStrictEqual(true)
   })
-  test('"{[]}" returns true', () => {
+  test.skip('"{[]}" returns true', () => {
     expect(isValidParentheses('{[]}')).toStrictEqual(true)
   })
-  test('"(]" returns false', () => {
+  test.skip('"(]" returns false', () => {
     expect(isValidParentheses('(]')).toStrictEqual(false)
   })
-  test('"([)]" returns false', () => {
+  test.skip('"([)]" returns false', () => {
     expect(isValidParentheses('([)]')).toStrictEqual(false)
   })
 })
