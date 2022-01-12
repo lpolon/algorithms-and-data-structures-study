c9a533d01cea815355ad90f2ba9345cbad97acbb
Leonardo Polon <leonardo.polon@b2wdigital.com>
2022-01-12 18:22:11 -0300

    wip: eslint config for practice

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  env: {
+    node: true,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+};
