2e3a0365f9f3f7d133437c58d510491f5e12eb2e
Leonardo Polon <leonardo.polon@b2wdigital.com>
2022-01-18 18:05:03 -0300

    3sum unguided attempt done

---
+++ src/8) 3Sum/notes.md
@@ -0,0 +1,9 @@
+# notes on unguided attempt
+
+based on two sum options, I guess we could have somewhat a hashtable with possible sums of each element and a second hashtable as in single pass solution... I understand it would be an exponential increase in space complexity.
+
+The naive aproach would be a three pointer method with O(n^3) in space complexity
+
+what I did in my unguided solution was a two pointer where I find the third value with binary search. In my understanding it would be O(n^(2 \* log n)). However, I was kinda desperate and added like two sorts and parsing array into strings... Very clumsy.
+
+I'm eager to watch Aaron's answer.
--- src/8) 3Sum/solution.test.ts
+++ src/8) 3Sum/solution.test.ts
@@ -1,6 +1,7 @@
-import { unguided__threeSum } from './solution';
+import { unguided__threeSum } from './unguided-solution';
+import { threeSum } from './solution';
 
-describe('unguided attempt', () => {
+xdescribe('unguided attempt', () => {
   test('should return expected output', () => {
     const input = [-1, 0, 1, 2, -1, -4];
     const expectedOutput = [
@@ -22,6 +23,32 @@ describe('unguided attempt', () => {
     const expectedOutput: number[][] = [];
 
     const output = unguided__threeSum(input);
+    expect(output).toEqual(expectedOutput);
+  });
+});
+
+describe("Aaron's solution", () => {
+  test('empty array should return no valid solution', () => {
+    const input: number[] = [];
+    const expectedOutput: number[][] = [];
+
+    const output = threeSum(input);
+    expect(output).toEqual(expectedOutput);
+  });
+  test('zero as only array element should return no valid solution', () => {
+    const input = [0];
+    const expectedOutput: number[][] = [];
+
+    const output = threeSum(input);
+    expect(output).toEqual(expectedOutput);
+  });
+  test('should return expected output', () => {
+    const input = [-1, 0, 1, 2, -1, -4];
+    const expectedOutput = [
+      [-1, -1, 2],
+      [-1, 0, 1],
+    ];
+    const output = threeSum(input);
     expect(output).toEqual(expectedOutput);
   });
 });
